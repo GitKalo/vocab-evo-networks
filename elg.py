50063b3d9b3b4f4bda51383df6eb601a3eba3c91
Kaloyan <kd1u18@soton.ac.uk>
2021-02-21 12:50:30 +0000

    Added matrices

--- elg.py
+++ elg.py
@@ -1,7 +1,11 @@
 class Agent :
+    N_objects = 5
+    N_symbols = 5
 
-    def __init__(self, id) :
+    def __init__(self, id, n_objects=N_objects, n_symbols=N_symbols) :
         self.id = id
+        self.P_matrix = [[round(1 / n_symbols, 2)] * n_symbols] * n_objects
+        self.Q_matrix = [[round(1 / n_objects, 2)] * n_objects] * n_symbols
 
     def talk(self, message) :
         print(message)
--- sim.py
+++ sim.py
@@ -3,4 +3,12 @@ import matplotlib.pyplot as plt
 
 import elg
 
-elg.Agent(1).talk('Hello')
+agent = elg.Agent(1, 3, 2)
+
+agent.talk('Hello')
+
+for r in agent.P_matrix :
+    print(r)
+
+for r in agent.Q_matrix :
+    print(r)
