2ac9423f6de0269dd2ea2a25c15c208f378efa51
Kaloyan <kd1u18@soton.ac.uk>
2021-02-28 19:35:53 +0000

    Added active and passive matrix derivation

--- elg.py
+++ elg.py
@@ -6,8 +6,8 @@ class Agent :
 
     def __init__(self, id, n_objects=N_objects, n_symbols=N_symbols) :
         self.id = id
-        self.active_matrix = [[round(1 / n_symbols, 2)] * n_symbols] * n_objects
-        self.passive_matrix = [[round(1 / n_objects, 2)] * n_objects] * n_symbols
+        self.active_matrix = [[0] * n_symbols] * n_objects
+        self.passive_matrix = [[0] * n_objects] * n_symbols
         self.assoc_matrix = [[0] * n_symbols] * n_objects
 
     def speak(self, listener) :
@@ -15,6 +15,22 @@ class Agent :
         obj = Agent.pick_list(self.active_matrix)
         sym = Agent.pick_item(self.active_matrix[obj])
         print("Agent", self.id, "is using symbol", sym, "to talk about object", obj)
+
+    def update_active_matrix(self) :
+        self.active_matrix = np.zeros(np.shape(self.assoc_matrix))
+
+        for i in range(len(self.active_matrix)) :
+            row_sum = sum(self.assoc_matrix[i])
+            for j in range(len(self.active_matrix[i])):
+                self.active_matrix[i][j] = self.assoc_matrix[i][j] / row_sum  # TODO
+
+    def update_passive_matrix(self) :
+        self.passive_matrix = np.zeros(tuple(reversed(np.shape(self.assoc_matrix))))
+
+        for j in range(len(self.passive_matrix)) :
+            col_sums = np.sum(self.assoc_matrix, axis=0)
+            for i in range(len(self.passive_matrix[j])):
+                self.passive_matrix[j][i] = self.assoc_matrix[i][j] / col_sums[j]  # TODO
 
     @staticmethod
     def pick_list(lists) :
@@ -35,6 +51,15 @@ class Agent :
                 count += 1
         except AssertionError :
             print("NO! Bad list!")  # TODO: Change error handline message (and add return value?)
+
+    def set_active_matrix(self, new_active_matrix) :
+        self.active_matrix = new_active_matrix
+
+    def set_passive_matrix(self, new_passive_matrix) :
+        self.passive_matrix = new_passive_matrix
+
+    def set_assoc_matrix(self, new_assoc_matrix) :
+        self.assoc_matrix = new_assoc_matrix
 
     def __str__(self) :
         return 'a[' + str(self.id) + ']'
