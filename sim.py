d431c7394608a58ced391bf75cd76f2fa27fc1b2
Kaloyan <kd1u18@soton.ac.uk>
2021-02-28 18:09:33 +0000

    Fix population generation

--- sim.py
+++ sim.py
@@ -19,13 +19,14 @@ def generation( G, agents ) :
     # return new graph
 
 if __name__ == '__main__' :
+    pop_size = 5
+
     agent_list = [elg.Agent(n) for n in range(pop_size)]
 
     G = nx.complete_graph(agent_list)
 
     nx.draw_circular(G, with_labels=True, font_weight='bold')
 
-    pop_size = len(agent_list)
     n_time_steps = 10
 
     for step_num in range(n_time_steps) :
