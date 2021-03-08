235de279ac19d8bc367cd382123b874c698d0ce7
Kaloyan <kd1u18@soton.ac.uk>
2021-03-08 23:51:47 +0000

    Add test for pick_item failure

--- test.py
+++ test.py
@@ -6,12 +6,16 @@ import unittest
 
 class TestUtil(unittest.TestCase) :
 
-    def test_pick_item(self) :
+    def test_pick_item_success(self) :
         list_items = np.random.randint(1, 10, size=10)
         sum_items = sum(list_items)
         list_prob = [i/sum_items for i in list_items]
         item = util.pick_item(list_prob)
         self.assertLess(item, len(list_prob))
+
+    def test_pick_item_failure(self) :
+        list_items = np.random.randint(1, 10, size=10)
+        self.assertRaises(AssertionError, util.pick_item, list_items)
 
 if __name__ == '__main__' :
     unittest.main()
