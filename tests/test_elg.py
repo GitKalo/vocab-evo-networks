e9c300cac78ffff02109b6cd76cd265798018ef4
Kaloyan <kd1u18@soton.ac.uk>
2021-03-10 09:01:55 +0000

    Add ELG test suite with stumps

---
+++ tests/test_elg.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+import unittest
+
+from context import src
+
+class TestELG(unittest.TestCase) :
+    def test_update_language(self) :
+        pass
+
+    def test_update_active_matrix_empty(self) :
+        # test empty assoc matrix
+        # test empty rows assoc matrix
+        pass
+
+    def test_update_active_matrix_small(self) :
+        pass
+
+    def test_update_active_matrix_large(self) :
+        pass
+
+    def test_update_passive_matrix_empty(self) :
+        # test empty assoc matrix
+        # test empty rows assoc matrix
+        pass
+
+    def test_update_passive_matrix_small(self) :
+        pass
+
+    def test_update_passive_matrix_large(self) :
+        pass
+
+    def test_random_assoc_matrix_shape(self) :
+        pass
+
+    def test_payoff_small(self) :
+        pass
+
+    def test_payoff_large(self) :
+        pass
+    
+    def test_payoff_symmetry(self) :
+        pass
+
+    def test_sample_none(self) :
+        pass
+
+    def test_sample_empty(self) :
+        pass
+
+    def test_sample_failed_pick_item(self) :
+        pass
+
+    def test_sample_small_square(self) :
+        pass
+
+    def test_sample_small_rect(self) :
+        pass
+
+    def test_sample_large_square(self) :
+        pass
+
+    def test_sample_large_rect(self) :
+        pass
+
+if __name__ == '__main__' :
+    unittest.main()
