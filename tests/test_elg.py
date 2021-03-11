b017761d75587aba15eace368fa54c142389e3da
Kaloyan <kd1u18@soton.ac.uk>
2021-03-11 09:11:11 +0000

    Add update_active_matrix tests

--- tests/test_elg.py
+++ tests/test_elg.py
@@ -35,15 +35,50 @@ class TestELG(unittest.TestCase) :
                 self.assertEqual(np.shape(self.agent.passive_matrix), shape[::-1])
 
     def test_update_active_matrix_empty(self) :
-        # test empty assoc matrix
-        # test empty rows assoc matrix
-        pass
+        empty_vals = [
+            [],
+            [[], [], []]
+        ]
+
+        for val in empty_vals :
+            with self.subTest(assoc_matrix=val) :
+                self.agent.set_assoc_matrix(val)
+                self.agent.update_active_matrix()
+                self.assertEqual(self.agent.active_matrix.size, 0)
+                self.assertEqual(np.shape(self.agent.active_matrix), np.shape(val))
 
     def test_update_active_matrix_small(self) :
-        pass
+        assoc_matrix = [
+            [3, 1],
+            [2, 8]
+        ]
+        expected_active_matrix = np.array([
+            [0.75, 0.25],
+            [0.2, 0.8]
+        ])
+
+        self.agent.set_assoc_matrix(assoc_matrix)
+        self.agent.update_active_matrix()
+        self.assertTrue(np.array_equal(self.agent.active_matrix, expected_active_matrix))
 
     def test_update_active_matrix_large(self) :
-        pass
+        assoc_matrix = [
+            [3, 1, 5, 6, 1, 4, 5],
+            [2, 7, 5, 1, 9, 0, 1],
+            [7, 4, 2, 0, 3, 7, 2],
+            [1, 0, 4, 5, 6, 3, 6]
+        ]
+        expected_active_matrix = np.array([
+            [0.12, 0.04, 0.2, 0.24, 0.04, 0.16, 0.2],
+            [0.08, 0.28, 0.2, 0.04, 0.36, 0, 0.04],
+            [0.28, 0.16, 0.08, 0, 0.12, 0.28, 0.08],
+            [0.04, 0, 0.16, 0.2, 0.24, 0.12, 0.24]
+        ])
+
+        self.agent.set_assoc_matrix(assoc_matrix)
+        self.agent.update_active_matrix()
+        self.assertTrue(np.array_equal(self.agent.active_matrix, expected_active_matrix))
+
 
     def test_update_passive_matrix_empty(self) :
         # test empty assoc matrix
