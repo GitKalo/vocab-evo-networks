8afa194b9f9a5839f409e66e81d782c6d604d37b
Kaloyan <kd1u18@soton.ac.uk>
2021-03-11 07:52:58 +0000

    Add update_language tests

--- tests/test_elg.py
+++ tests/test_elg.py
@@ -8,6 +8,32 @@ class TestELG(unittest.TestCase) :
     def setUp(self) :
         self.agent = src.elg.Agent(0)
 
+    def test_update_language_empty(self) :
+        empty_vals = [None, []]
+        for val in empty_vals :
+            with self.subTest(assoc_matrix=val) :
+                self.agent.update_language(val)
+                self.assertEqual(self.agent.assoc_matrix, [])
+                self.assertEqual(self.agent.active_matrix.size, 0)
+                self.assertEqual(self.agent.passive_matrix.size, 0)
+
+    def test_update_language_shape(self) :
+        shapes = [
+            (1, 1),
+            (5, 5),
+            (100, 100),
+            (1, 10),
+            (5, 100),
+            (10, 1),
+            (100, 5)
+        ]
+
+        for shape in shapes :
+            with self.subTest(assoc_matrix_shape=shape) :
+                self.agent.update_language(np.zeros(shape))
+                self.assertEqual(np.shape(self.agent.active_matrix), shape)
+                self.assertEqual(np.shape(self.agent.passive_matrix), shape[::-1])
+
     def test_update_active_matrix_empty(self) :
         # test empty assoc matrix
         # test empty rows assoc matrix
