e029bdf9540a63c4d00fad7d2c68aaf55b9d7ac3
GitKalo <mynameis.kaloyan@gmail.com>
2021-08-18 17:53:47 +0300

    Move import results func to analysis script

---
+++ analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+import sys, ast
+
+# Import CSV of simulation results
+def import_results(results_filepath) :
+    try :
+        filetype = results_filepath.split('.')[-1]
+        if filetype == 'csv' :
+            imported_df = pd.read_csv(results_filepath, index_col=0, converters={
+                'avg_payoffs': ast.literal_eval,
+                'node_payoffs': ast.literal_eval
+                })
+        elif filetype in ['parquet', 'parq'] :
+            imported_df = pd.read_parquet(results_filepath)
+        else :
+            raise BaseException(f"Unrecognized file type '{filetype}'.")
+        
+        return imported_df
+    except FileNotFoundError :
+        print(f"Could not find the results file '{results_filepath}'. Exiting...")
+        raise
+    except BaseException as e :
+        print(e)
+        print("Exiting...")
+        sys.exit()
--- plotting.py
+++ plotting.py
@@ -72,29 +72,6 @@ def plot_node_payoffs(ax, runs, i_run=0, type='line', time_step=0) :
 
     return ax
 
-# Import CSV of simulation results
-def import_results(results_filepath) :
-    try :
-        filetype = results_filepath.split('.')[-1]
-        if filetype == 'csv' :
-            imported_df = pd.read_csv(results_filepath, index_col=0, converters={
-                'avg_payoffs': ast.literal_eval,
-                'node_payoffs': ast.literal_eval
-                })
-        elif filetype in ['parquet', 'parq'] :
-            imported_df = pd.read_parquet(results_filepath)
-        else :
-            raise BaseException(f"Unrecognized file type '{filetype}'.")
-        
-        return imported_df
-    except FileNotFoundError :
-        print(f"Could not find the results file '{results_filepath}'. Exiting...")
-        raise
-    except BaseException as e :
-        print(e)
-        print("Exiting...")
-        sys.exit()
-
 if __name__ == '__main__' :
     # Get results filename
     try :
