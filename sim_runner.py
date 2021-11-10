1b49374911b84a9d0352ec9a34e02ae73c5d17f7
GitKalo <mynameis.kaloyan@gmail.com>
2021-11-10 19:00:20 +0200

    Remove debugging print statements

--- sim_runner.py
+++ sim_runner.py
@@ -53,8 +53,6 @@ if __name__ == '__main__' :
     parser.add_argument('--include-langs', action='store_true')
 
     args = parser.parse_args()
-    print(args)
-    print(args.include_langs)
 
     # Get parameter filename
     try :
